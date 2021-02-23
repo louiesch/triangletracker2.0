5d15f9c89bbe5d18ee456f6c9fb15fd9e2a961d2
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-23 14:44:56 -0800

    write red stage test for determining scalene triangle
    Co-authored by: Mike Pingel <mdpingel@gmail.com>

--- Triangle.Tests/ModelTests/TriangleTrackerTests.cs
+++ Triangle.Tests/ModelTests/TriangleTrackerTests.cs
@@ -7,17 +7,24 @@ namespace Triangle.Tests
   public class TriangleTrackerTests
   {
     [TestMethod]
-    public void IsTriangle_AnySideLongerThanOtherTwoCombined_False() //NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void IsTriangle_AnySideLongerThanOtherTwoCombined_NotATriangle() //NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
     {
       TriangleTracker testTriangle = new TriangleTracker();
       Assert.AreEqual("not a triangle", testTriangle.IsTriangle(2, 2, 8));
     }
 
     [TestMethod]
-    public void IsTriangle_CanMakeTriangle_True()
+    public void IsTriangle_CanMakeTriangle_IsATriangle()
     {
       TriangleTracker testTriangle = new TriangleTracker();
       Assert.AreEqual("is a triangle", testTriangle.IsTriangle(2, 2, 3));
+    }
+
+    [TestMethod]
+    public void IsTriangle_IsTriangleScalene_IsScalene()
+    {
+      TriangleTracker testTriangle = new TriangleTracker();
+      Assert.AreEqual("scalene triangle", testTriangle.IsTriangle(2, 3, 4));
     }
   }
 }
