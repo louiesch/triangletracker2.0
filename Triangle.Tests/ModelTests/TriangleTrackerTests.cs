8f5602d20e458c158df417ebee50e37c10f7334b
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-23 13:40:09 -0800

    set up all required files for triangle tracker program and tests in csharp

---
+++ Triangle.Tests/ModelTests/TriangleTrackerTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace Triangle.Tests
+{
+  [TestClass]
+  public class TriangleTrackerTests
+  {
+    [TestMethod]
+    //test goes here
+  }
+}
---
+++ Triangle/Models/TriangleTracker.cs
@@ -0,0 +1,11 @@
+namespace Triangle
+{
+  public class TriangleTracker
+  {
+    public bool IsTriangle(int side1, int side2, int side3)
+    {
+      //logic here
+      return false;
+    }
+  }
+}
